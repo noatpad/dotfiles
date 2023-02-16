6715186e24251a6c9570c973ae344d890cd1d664
Danny Hernandez <tamdannyhdz@gmail.com>
2023-02-15 21:24:37 -0600

    Feb 15 update

---
+++ .zshrc
@@ -0,0 +1,6 @@
+export ZSH="$HOME/.oh-my-zsh"
+export DOT="$HOME/.dotfiles"
+
+source $DOT/zsh/config
+source $DOT/zsh/plugins
+source $ZSH/oh-my-zsh.sh
--- install.sh
+++ install.sh
@@ -1,12 +1,19 @@
 #!/usr/bin/env bash
 
-# Prepare package installations
-sudo apt-add-repository ppa:git-core/ppa	# Latest git
-sudo add-apt-repository ppa:spvkgn/exa		# exa for Ubunty 20.04
-curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null		# gh
-echo "deb http://packages.azlux.fr/debian/ buster main" | sudo tee /etc/apt/sources.list.d/azlux.list
-wget -qO - https://azlux.fr/repo.gpg.key | sudo apt-key add -		# gping
+## PREPARE INSTALLATIONS ##
+# Repositories for: git, wslu
+sudo apt-add-repository ppa:git-core/ppa
+sudo add-apt-repository ppa:wslutilities/wslu
+# gh
+type -p curl >/dev/null || sudo apt install curl -y
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
+&& sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
+&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+# gping
+echo "deb [signed-by=/usr/share/keyrings/azlux-archive-keyring.gpg] http://packages.azlux.fr/debian/ stable main" | sudo tee /etc/apt/sources.list.d/azlux.list
+sudo wget -O /usr/share/keyrings/azlux-archive-keyring.gpg  https://azlux.fr/repo.gpg
+# speedtest
+curl -s https://packagecloud.io/install/repositories/ookla/speedtest-cli/script.deb.sh | sudo bash
 
 sudo apt-get update
 
@@ -23,20 +30,22 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 # Install asdf
-git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.9.0
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.11.1
 
 # Install packages
 sudo apt install exa
 sudo apt install ffmpeg
 sudo apt install gh
+sudo apt install gifsicle
 sudo apt install gping
 sudo apt install httpie
-curl https://getmic.ro | bash
+sudo apt install lua5.3
+curl https://getmic.ro | bash   # micro
 sudo apt install neofetch
-sh -c "$(curl -fsSL https://starship.rs/install.sh)"
-sudo curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl
-sudo chmod a+rx /usr/local/bin/youtube-dl
-curl -sS https://webinstall.dev/zoxide | bash
+sudo apt install speedtest
+sh -c "$(curl -fsSL https://starship.rs/install.sh)"  # starship
+sudo curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl && sudo chmod a+rx /usr/local/bin/youtube-dl  # youtube-dl
+curl -sS https://webinstall.dev/zoxide | bash   # zoxide
 
 sudo apt-get install make build-essential libssl-dev zlib1g-dev \
 libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
@@ -47,4 +56,4 @@ libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-d
 
 # Prepare data and symlinks
 cp gitconfig ~/.gitconfig
-ln -svf ~/.zshrc /mnt/c/Users/Danny/zsh/zshrc
+ln -sf ~/.zshrc ./zsh/zshrc
